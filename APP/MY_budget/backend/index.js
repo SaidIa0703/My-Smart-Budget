51377644d503ff9e796601c8472e9505d6ec5442
abdel <saidiabdelghani0703@gmail.com>
2026-03-17 20:45:01 +0100

    feat: add auth, transactions, questionnaire

--- APP/MY_budget/backend/index.js
+++ APP/MY_budget/backend/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const { db, connectMongo, testPostgres } = require('./db');
-
+const authRoutes = require('./routes/auth');
+const profileRoutes = require('./routes/profile');
+const transactionsRoutes = require('./routes/transactions');
 const app = express();
 
 // Middleware
 app.use(cors());
+app.use('/api/profile', profileRoutes);
 app.use(express.json());
+app.use('/api/transactions', transactionsRoutes);
+app.use('/api/auth', authRoutes);
 
 // Connecter MongoDB et PostgreSQL au démarrage
 connectMongo();
@@ -135,8 +140,9 @@ app.get('/', (req, res) => {
 });
 
 // Démarrer le serveur
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`🚀 Serveur lancé sur le port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
 });
+

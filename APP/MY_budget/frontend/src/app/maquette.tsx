157d7dae45bc25e15c309ec41326988fa3fd357b
abdel <saidiabdelghani0703@gmail.com>
2026-03-18 15:25:54 +0100

    fix: recharts + type error PieLabelRenderProps

--- APP/MY_budget/frontend/src/app/maquette.tsx
+++ APP/MY_budget/frontend/src/app/maquette.tsx
@@ -226,7 +226,7 @@ const MySmartBudget = () => {
                   cx="50%"
                   cy="50%"
                   labelLine={false}
-                  label={({ name, spent }) => `${name}: €${spent}`}
+                  label={(props: any) => `${props.name}: €${props.spent}`}
                   outerRadius={100}
                   fill="#8884d8"
                   dataKey="spent"
